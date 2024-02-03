ffc2ee78e8273b18960149d92ff3e6455231508a
Deepak Suyal <deepaksuyal310@gmail.com>
2024-02-03 08:03:16 +0530

    My First Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 80;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static', express.static('static')) // for serving static files
+app.use(express.urlencoded({ extended:true}))
+
+// PUG SPECIFIC STUFF/CONFIGURATION
+app.set('view engine', 'pug') // set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+//END POINTS
+    app.get("/", (req, res)=>{       
+        const params = {}
+        res.status(200).render('home.pug', params);
+});
+
+    app.get("/contact", (req, res)=>{       
+        const params = {}
+        res.status(200).render('contact.pug', params);
+});
+
+//START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
